974de7da608b938d686b33deb2e802af1d9fa7b2
visionaicom <aibusinesswork0@gmail.com>
2026-04-02 00:17:27 +0530

    Update _supabase.js

--- _supabase.js
+++ _supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  "https://bvphejopafojkortmzjc.supabase.co",
+  "sb_publishable_DAy-jxY5mlwv_kZ2cQYzxA_lZ6aG5ZX"
 );
 
 export default supabase;
